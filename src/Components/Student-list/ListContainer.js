496f9106bcd957a9ac9a7ecd97a55303b890d078
Your Name <ju679935@gmail.com>
2022-09-29 02:29:22 +0600

    cart section completed

--- src/Components/Student-list/ListContainer.js
+++ src/Components/Student-list/ListContainer.js
@@ -25,23 +25,28 @@ const ListContainer = () => {
            </div>
 
            <div className="container cart">
-               <div className="author-info">
-               <h4 className='mt-5 font-bold'>Jalal Uddin Emon </h4>
-               <p className='w-17 f-bold'>Chattogram, Bangladesh</p>
-               <div className="others-info">
-                <h5>wieght : 55 </h5>
-                <h5>age : 22 </h5>
-                <h5>hieght : 5.5 </h5>
+                <div className="author-info">
+                <h4 className='mt-5 font-bold' class='mt-5 text-primary'>Jalal Uddin Emon </h4>
+                <p className='w-17 f-bold'>Chattogram, Bangladesh</p>
+                    <div className="others-info">
+                        <h5>wieght : 55 </h5>
+                        <h5>age : 22 </h5>
+                        <h5>hieght : 5.5 </h5>
 
-               </div>
-               <div className=" font-weight-bold mt-5 add-break">
-                <h4>Add a Break</h4>
-                <button className='btn btn-info'>10s</button>
-                <button className='btn btn-info'>20s</button>
-                <button className='btn btn-info'>30s</button>
-                <button className='btn btn-info'>40s</button>
-               </div>
-
+                    </div>
+                    <div className=" font-weight-bold mt-5 add-break">
+                        <h4>Add a Break:</h4>
+                        <button className='btn btn-info'>10s</button>
+                        <button className='btn btn-info'>20s</button>
+                        <button className='btn btn-info'>30s</button>
+                        <button className='btn btn-info'>40s</button>
+                    </div>
+                    <div className="excercise-details bg-white pb-8 mt-5">
+                        <h4>Reading Details:</h4>
+                        <h5 className='mt-5'><span>Reading time:</span> <span>00</span>m </h5>
+                        <h5 className='mt-5'><span>Break time:</span> <span>00</span>s </h5>
+                    </div>
+                <button  type="button" class="btn btn-primary mt-2 w-100">Activity completed</button>
                </div>
 
 

a686d404dfaff1a4c6d1086ea438ee146ec32b03
Your Name <ju679935@gmail.com>
2022-09-29 05:12:02 +0600

    second clickable

--- src/Components/Student-list/ListContainer.js
+++ src/Components/Student-list/ListContainer.js
@@ -24,6 +24,34 @@ const ListContainer = () => {
     for (const students of cart ){
         total = total + students.time;
     }
+
+    const timerSeconds = [10, 20, 30, 40];
+    const [seconds, setSeconds] = useState([]);
+    
+    let timeAdd = 0;
+    const secondHandler10 = (timerSeconds) => {
+    const newTime = parseInt(timeAdd) + timerSeconds[0]
+    setSeconds(newTime)
+  
+    }
+
+    const secondHandler20 = (timerSeconds) => {
+        const newTime = parseInt(timeAdd) + timerSeconds[1]
+        setSeconds(newTime)
+       
+    }
+    const secondHandler30 = (timerSeconds) => {
+        const newTime = parseInt(timeAdd) + timerSeconds[2]
+        setSeconds(newTime)
+       
+    }
+    const secondHandler40 = (timerSeconds) => {
+        const newTime = parseInt(timeAdd) + timerSeconds[3]
+        setSeconds(newTime)
+       
+    }
+
+
     return (
         <div className='list container mt-5'>
            <div className=' container student-list'>
@@ -42,6 +70,7 @@ const ListContainer = () => {
 
            <div className="container cart">
                 <div className="author-info">
+                    <h6>Your passionate Teacher</h6>
                 <h4 className='mt-5 font-bold' class='mt-5 text-primary'>Jalal Uddin Emon </h4>
                 <p className='w-17 f-bold'>Chattogram, Bangladesh</p>
                     <div className="others-info">
@@ -52,15 +81,15 @@ const ListContainer = () => {
                     </div>
                     <div className=" font-weight-bold mt-5 add-break">
                         <h4>Add a Break:</h4>
-                        <button className='btn btn-info'>10s</button>
-                        <button className='btn btn-info'>20s</button>
-                        <button className='btn btn-info'>30s</button>
-                        <button className='btn btn-info'>40s</button>
+                        <button onClick={() =>secondHandler10 (timerSeconds)} className='btn btn-info' >10s </button>
+                        <button onClick={() => secondHandler20(timerSeconds)} className='btn btn-info'>20s</button>
+                        <button onClick={() => secondHandler30(timerSeconds)} className='btn btn-info'>30s</button>
+                        <button onClick={() => secondHandler40(timerSeconds)} className='btn btn-info'>40s</button>
                     </div>
                     <div className="excercise-details bg-white pb-8 mt-5">
                         <h4>Reading Details:</h4>
                         <h5 className='mt-5'><span>Reading time:</span> <span className='text-primary'>{total}</span>m </h5>
-                        <h5 className='mt-5'><span>Break time:</span> <span>00</span>s </h5>
+                    <h5 className='mt-5'><span>Break time:</span> <span>{seconds}</span>s </h5>
                     </div>
                 <button  type="button" class="btn btn-primary mt-2 w-100">Activity completed</button>
                </div>
